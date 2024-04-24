7b30f47a4a1edca5194f654dd476cf8795829faa
EAJ <0xMuTwo@gmail.com>
2024-04-24 02:05:25 -0700

    navbar tweak

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,23 @@
+"use client";
+import { useAppStateStore } from "@/stores/useAppStateStore";
+import { AppState } from "./types";
+
 const Navbar = () => {
+  const appState = useAppStateStore((state) => state.appState);
+
+  const translateAppState = (appState: AppState) => {
+    if (appState === "Intro" || appState === "IP") return "A Builder";
+    if (appState === "LP1") return "A Liquidity Provider";
+    if (appState === "LP2") return "A Secondary Buyer";
+    return "Builder"; //Default Case
+  };
+
   return (
-    <nav className="sticky flex justify-between items-center py-2 px-4 md:px-8 lg:px-16 top-0  h-auto text-lg md:text-xl w-full lg:text-2xl">
+    <nav className="sticky flex justify-between items-center py-2 px-4 md:px-8 lg:px-16 top-0 h-auto text-lg md:text-xl w-full lg:text-2xl">
       <div>Royco</div>
-      <div>List Assets</div>
+      <div>You are: {translateAppState(appState)}</div>
     </nav>
   );
 };
+
 export default Navbar;
